67f82c2e155573a41ffb3bd4197f8ff22d8819c9
rohini287 <74919500+rohini287@users.noreply.github.com>
2021-01-16 11:10:21 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,26 @@
+class Box{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0,
+            'friction':0.3,
+            'density':1.0
+        }
+        this.x=x
+        this.y=y
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+       
+        World.add(world, this.body);
+      }
+      display(){
+        var pos=this.body.position
+      
+        push();
+        translate(this.body.position.x, this.body.position.y);
+       
+        rectMode(CENTER)
+        rect(0,0, this.width, this.height);
+        pop();
+      }
+}
